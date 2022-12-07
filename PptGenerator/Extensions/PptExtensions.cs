cb80ca9b6d086b19446f31c1c0c0cc72758c689e
Bruno Rossi Gavioli <brunorgavioli@gmail.com>
2022-12-07 15:41:22 -0300

    Melhoria e "cadastro" de informações básicas para o powerpoint

---
+++ PptGenerator/Extensions/PptExtensions.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Office.Core;
+using Microsoft.Office.Interop.PowerPoint;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PptGenerator.Extensions
+{
+    public static class PptExtensions
+    {
+        private static readonly string _basePath = @"D:\Desenvolvimento\Aprendizado\Office.Interop\PptGenerator\PptGenerator";
+        private static readonly string _modelosPath = Path.Combine(_basePath, "FileModel");
+        private static readonly string _processadosPath = Path.Combine(_basePath, "Processados");
+        private static CustomLayout _customLayout = default!;
+
+        public static Presentation IniciarPowerPoint(string modeloPpt)
+        {
+            if (!Directory.Exists(_processadosPath))
+                Directory.CreateDirectory(_processadosPath);
+
+            if (!Directory.Exists(_modelosPath))
+                Directory.CreateDirectory(_modelosPath);
+
+            var baseFilePath = Path.Combine(_modelosPath, modeloPpt + ".pptx");
+            var ppt = new Application();
+            var presentation = ppt.Presentations.Open(baseFilePath, WithWindow: MsoTriState.msoCTrue);
+            presentation.Application.WindowState = PpWindowState.ppWindowMinimized;
+            _customLayout = presentation.SlideMaster.CustomLayouts[1];
+            return presentation;
+        }
+
+        public static void SalvarComo(this Presentation presentation)
+        {
+            presentation.Application.WindowState = PpWindowState.ppWindowNormal;
+            presentation.SaveAs(Path.Combine(_processadosPath, $"Apresentacao_{DateTime.Now:ddMMyyyyHHmmss}"));
+        }
+
+        public static Slide AdicionarSlide(this Slides slides, int? index = null, CustomLayout? pCustomLayout = null)
+        {
+            return slides.AddSlide(index ?? slides.Count + 1, pCustomLayout ?? _customLayout);
+        }
+
+        public static void DefinirValor(this Cell cell, object? value)
+        {
+            try
+            {
+                cell.Shape.TextFrame.TextRange.Text = value?.ToString() ?? string.Empty;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                cell.Shape.TextFrame.TextRange.Text = string.Empty;
+            }
+        }
+    }
+}
--- PptGenerator/Program.cs
+++ PptGenerator/Program.cs
@@ -1,35 +1,60 @@
-﻿using Microsoft.Office.Core;
-using Microsoft.Office.Interop.PowerPoint;
+﻿using Microsoft.Office.Interop.PowerPoint;
+using PptGenerator.Extensions;
+using System.Globalization;
 
-
-internal class Program
+namespace PptGenerator
 {
-    private static void Main(string[] args)
+    public class Program
     {
-        var basePath = @"D:\Desenvolvimento\Aprendizado\Office.Interop\PptGenerator\PptGenerator";
-
-        var modelosPath = Path.Combine(basePath, "FileModel");
-        var processadosPath = Path.Combine(basePath, "Processados");
-        if (!Directory.Exists(processadosPath))
-            Directory.CreateDirectory(processadosPath);
-        if (!Directory.Exists(modelosPath))
-            Directory.CreateDirectory(modelosPath);
-
-        var baseFilePath = Path.Combine(modelosPath, "ModeloMonitoramento.pptx");
-        var ppt = new Application
+        private static void Main(string[] args)
         {
-            Visible = MsoTriState.msoTrue,
-            WindowState = PpWindowState.ppWindowNormal
-        };
-        var presentation = ppt.Presentations.Open(baseFilePath, WithWindow: MsoTriState.msoCTrue);
-        var customLayout = presentation.SlideMaster.CustomLayouts[1];
-        var slide = presentation.Slides.AddSlide(1, customLayout);
-        var table = slide.Shapes.AddTable(5, 8).Table;
-        var col = 1;
-        foreach (Cell cell in table.Rows[1].Cells)
-        {
-            cell.Shape.TextFrame.TextRange.Text = col++.ToString();
+            Console.WriteLine("Quantos clientes vão ser adicionados?");
+            if (!int.TryParse(Console.ReadLine(), out var linhas))
+            {
+                Console.WriteLine("Caracter inválido, encerrando aplicação...");
+                return;
+            }
+
+            var presentation = PptExtensions.IniciarPowerPoint("ModeloMonitoramento");
+            var slide = presentation.Slides.AdicionarSlide();
+            var table = slide.Shapes.AddTable(linhas+1, 4).Table;
+
+            Dictionary<int, string> headers = new() { { 1, "Nome" }, { 2, "Idade" }, { 3, "Renda Mensal" }, { 4, "Renda Anual" } };
+            for (int i = 1; i <= table.Rows[1].Cells.Count; i++)
+                table.Rows[1].Cells[i].DefinirValor(headers[i]);
+
+            int clienteNum = 1;
+            foreach (Row row in table.Rows)
+            {
+                if (row == table.Rows[1])
+                    continue;
+
+                Console.Clear();
+                Console.WriteLine($"Cliente - {clienteNum++}\n");
+                for (int i = 1; i <= row.Cells.Count; i++)
+                {
+                    if (i == 1)
+                    {
+                        Console.WriteLine("Informe o nome do cliente");
+                        row.Cells[i].DefinirValor(Console.ReadLine());
+                    }
+                    else if (i == 2)
+                    {
+                        Console.WriteLine("Informe a idade do cliente");
+                        row.Cells[i].DefinirValor(Console.ReadLine());
+                    }
+                    else if (i == 3)
+                    {
+                        Console.WriteLine("Informe a renda mensal do cliente");
+                        if (!decimal.TryParse(Console.ReadLine(), out var valor))
+                            continue;
+                        row.Cells[i].DefinirValor(valor.ToString("C", CultureInfo.CurrentCulture));
+                        row.Cells[i+1].DefinirValor((valor * 12).ToString("C", CultureInfo.CurrentCulture));
+                    }
+                }
+            }
+
+            presentation.SalvarComo();
         }
-        presentation.SaveAs(Path.Combine(processadosPath, $"Apresentacao_{DateTime.Now:ddMMyyyyHHmmss}"));
     }
 }
